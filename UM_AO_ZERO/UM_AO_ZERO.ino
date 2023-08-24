3f5048ce6606eb7bb20af1dc0d714c345ba0a053
emiliasonalio <emiliasonalio@gmail.com>
2023-08-23 22:08:21 -0300

    Aula 23/08

---
+++ UM_AO_ZERO/UM_AO_ZERO.ino
@@ -0,0 +1,126 @@
+void setup() {
+  pinMode (13, OUTPUT);
+  pinMode (12, OUTPUT);
+  pinMode (11, OUTPUT);
+  pinMode (10, OUTPUT);
+  pinMode (9, OUTPUT);
+  pinMode (8, OUTPUT);
+  pinMode (7, OUTPUT);
+  pinMode (6, OUTPUT);
+}
+
+void loop() {
+ 
+ //NUMERO 1
+ digitalWrite(13, LOW);
+ digitalWrite(12, LOW);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, LOW);
+ digitalWrite(8, LOW);
+ digitalWrite(7, LOW);
+ digitalWrite(6, HIGH);
+ delay(500);
+ 
+ //NUMERO 2
+ digitalWrite(13, HIGH);
+ digitalWrite(12, HIGH);
+ digitalWrite(11, LOW);
+ digitalWrite(10, LOW);
+ digitalWrite(9, HIGH);
+ digitalWrite(8, LOW);
+ digitalWrite(7, HIGH);
+ digitalWrite(6, HIGH);
+ delay(500);
+
+ //NUMERO 3
+ digitalWrite(13, LOW);
+ digitalWrite(12, HIGH);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, HIGH);
+ digitalWrite(8, LOW);
+ digitalWrite(7, HIGH);
+ digitalWrite(6, HIGH);
+ delay(500);
+
+ 
+ //NUMERO 4
+ digitalWrite(13, LOW);
+ digitalWrite(12, LOW);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, HIGH);
+ digitalWrite(8, HIGH);
+ digitalWrite(7, LOW);
+ digitalWrite(6, HIGH);
+ delay(500);
+
+ //NUMERO 5
+ digitalWrite(13, LOW);
+ digitalWrite(12, HIGH);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, HIGH);
+ digitalWrite(8, HIGH);
+ digitalWrite(7, HIGH);
+ digitalWrite(6, LOW);
+ delay(500);
+
+ //NUMERO 6
+ digitalWrite(13, HIGH);
+ digitalWrite(12, HIGH);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, HIGH);
+ digitalWrite(8, HIGH);
+ digitalWrite(7, HIGH);
+ digitalWrite(6, LOW);
+ delay(500);
+ 
+ //NUMERO 7
+ digitalWrite(13, LOW);
+ digitalWrite(12, LOW);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, LOW); 
+ digitalWrite(8, LOW);
+ digitalWrite(7, HIGH);
+ digitalWrite(6, HIGH);
+ delay(500);
+
+ //NUMERO 8
+ digitalWrite(13, HIGH);
+ digitalWrite(12, HIGH);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, HIGH); 
+ digitalWrite(8, HIGH);
+ digitalWrite(7, HIGH);
+ digitalWrite(6, HIGH);
+ delay(500);
+
+  //NUMERO 9
+ digitalWrite(13, LOW);
+ digitalWrite(12, LOW);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, HIGH); 
+ digitalWrite(8, HIGH);
+ digitalWrite(7, HIGH);
+ digitalWrite(6, HIGH);
+ delay(500);
+
+ //NUMERO 0
+ digitalWrite(13, HIGH);
+ digitalWrite(12, HIGH);
+ digitalWrite(11, HIGH);
+ digitalWrite(10, LOW);
+ digitalWrite(9, LOW); 
+ digitalWrite(8, HIGH);
+ digitalWrite(7, HIGH);
+ digitalWrite(6, HIGH);
+ delay(500);
+ 
+ 
+}
